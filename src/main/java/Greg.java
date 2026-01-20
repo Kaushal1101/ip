c172e81422b669142f26449b870ea17df95c065c
Kaushal Jayapragash <kaushaljayapragash@gmail.com>
2026-01-20 21:56:46 +0800

    Add marking feature

--- src/main/java/Greg.java
+++ src/main/java/Greg.java
@@ -16,23 +16,38 @@ public class Greg {
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[MAX];
+        Task[] tasks = new Task[MAX];
         int counter = 0;
 
         while (true) {
             String line = scanner.nextLine().trim();
             if (line.equals("bye")) {
+                // To exit the program
                 System.out.println(myOutro);
                 scanner.close();
                 break;
+            } else if (line.startsWith("mark ") || line.startsWith("unmark ")){
+                // To mark/unmark list items
+
+                // Minus one because list is 1 indexed, but array is 0 indexed
+                int index = Integer.parseInt(line.split(" ")[1]) - 1;
+                if (line.startsWith("mark")) {
+                    tasks[index].mark(true);
+                    System.out.println("Nice! I've marked this task as done: \n" + tasks[index].toString() );
+                } else {
+                    tasks[index].mark(false);
+                    System.out.println(" OK, I've marked this task as not done yet: \n" + tasks[index].toString() );
+                }
             } else if (line.equals("list")){
+                // To list task items
                 int i = 0;
                 while (tasks[i] != null) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ". " + tasks[i].toString());
                     i++;
                 }
             } else {
-                tasks[counter] = line;
+                // To add task
+                tasks[counter] = new Task(line);
                 counter++;
                 System.out.println("Greg added: " + line);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    String description;
+    boolean marked;
+
+    public Task(String description) {
+        this.description = description;
+        this.marked = false;
+    }
+
+    public void mark(boolean val) {
+        this.marked = val;
+    }
+
+    public String toString() {
+        String cross = marked ? "X" : " ";
+        return "[" + cross + "] " + description;
+    }
+}
