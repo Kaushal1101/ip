ad5ba82abf9850b7a60321f99642452231250f59
Kaushal Jayapragash <kaushaljayapragash@gmail.com>
2026-01-20 21:39:26 +0800

    Add list

--- src/main/java/Greg.java
+++ src/main/java/Greg.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Greg {
     public static void main(String[] args) {
+        int MAX = 100;
+
         String myIntro = " ____________________________________________________________\n" +
                 " Hello! I'm Greg\n" +
                 " What can I do for you?\n";
@@ -14,15 +16,25 @@ public class Greg {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[MAX];
+        int counter = 0;
+
         while (true) {
             String line = scanner.nextLine().trim();
-
             if (line.equals("bye")) {
                 System.out.println(myOutro);
                 scanner.close();
                 break;
+            } else if (line.equals("list")){
+                int i = 0;
+                while (tasks[i] != null) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                    i++;
+                }
             } else {
-                System.out.println("Greg says " + line);
+                tasks[counter] = line;
+                counter++;
+                System.out.println("Greg added: " + line);
             }
         }
     }
