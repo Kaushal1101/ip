10cbe36cddbf743ec86f5b1757060a764e1605ad
Kaushal Jayapragash <kaushaljayapragash@gmail.com>
2026-01-30 14:10:51 +0800

    Change input format to strict date and time

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,48 @@
+import java.time.LocalDate;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    private LocalDate fromDate;
+    private String fromTime; // "" if not provided
+    private LocalDate toDate;
+    private String toTime;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, String fromRaw, String toRaw) throws GregException {
         super(description);
-        this.from = from;
-        this.to = to;
+
+        this.fromDate = parseDate(fromRaw);
+        this.fromTime = parseOptionalTime(fromRaw);
+
+        this.toDate = parseDate(toRaw);
+        this.toTime = parseOptionalTime(toRaw);
     }
+
+//    @Override
+//    public String toString() {
+//        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+//    }
+//
+//    @Override
+//    public String toSaveString() {
+//        return "E | " + (marked ? 1 : 0) + " | " + description + " | " + " from: " + from + " - " + to;
+//    }
 
     @Override
     public String toString() {
+        String from = fromTime.isEmpty() ? fromDate.toString() : fromDate + " " + fromTime;
+        String to = toTime.isEmpty() ? toDate.toString() : toDate + " " + toTime;
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 
     @Override
     public String toSaveString() {
-        return "E | " + (marked ? 1 : 0) + " | " + description + " | " + " from: " + from + " - " + to;
+        // E | done | desc | fromDate | fromTime | toDate | toTime
+        return "E | " + (marked ? 1 : 0)
+                + " | " + description
+                + " | " + fromDate
+                + " | " + fromTime
+                + " | " + toDate
+                + " | " + toTime;
     }
 }
+
